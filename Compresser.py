0fc614e40c6abd6c4eacd71d500cb0255b8a21bc
Salimalkharsa <salimalkharsa@gmail.com>
2023-04-05 18:07:58 -0500

    Added a key word extraction functionality into the compresser

--- Compresser.py
+++ Compresser.py
@@ -1,23 +1,53 @@
-from transformers import AutoTokenizer, AutoModelWithLMHead
+from transformers import pipeline
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from itertools import combinations
+import networkx as nx
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 
 class Compresser:
     def __init__(self, text):
         self.text = text
         self.mask_token = '[MASK]' # this is the token that will be replaced by the model
-        self.summary = None
+        '''
+        self.summary = self.summarize()
+        self.words = self.tokenize()
+        self.to_mask = self.get_keywords()
+        '''
+        self.words_ = self.tokenize()
+        self.key_words_ = self.get_keywords()
 
     def summarize(self):
-        # Use GPT to generate a summary
-        tokenizer = AutoTokenizer.from_pretrained("gpt2")
-        model = AutoModelWithLMHead.from_pretrained("gpt2")
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        summary = summarizer(self.text, max_length=100, min_length=3, do_sample=False)
+        return summary[0]['summary_text']
+    
+    def tokenize(self):
+        # Tokenize text into sentences and words
+        sentences = sent_tokenize(self.text)
+        words = [word_tokenize(sentence.lower()) for sentence in sentences]
+        # Remove stop words and punctuation
+        stop_words = set(stopwords.words('english'))
+        words = [[word for word in sentence if word.isalnum() and word not in stop_words] for sentence in words]
+        return words
 
-        input_ids = tokenizer.encode(self.text, return_tensors="pt")
-        summary_ids = model.generate(input_ids, max_length=100, min_length=30, num_beams=2, no_repeat_ngram_size=2)
+    def get_keywords(self):
+        graph = nx.Graph()
+        for sentence in self.words_:
+            for u, v in combinations(sentence, 2):
+                if not graph.has_edge(u, v):
+                    graph.add_edge(u, v, weight=1)
+                else:
+                    graph.edges[u, v]['weight'] += 1
+        scores = nx.pagerank(graph, weight='weight')
+        threshold = 0.1 # adjust this threshold as desired
+        to_mask = [word for word, score in scores.items() if score < threshold]
+        return to_mask
 
-        # Convert summary IDs to text
-        self.summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-
-text = "Produce a short summary on this"
+#Just testing rq
+text = "The quick brown fox jumps over the lazy dog to eat the dog's food. The dog is now hungry and wants to eat the fox. The fox is now dead. The end."
 compresser = Compresser(text)
-compresser.summarize()
-print(compresser.summary)
+words = compresser.key_words_
+print(words)
--- Tester.py
+++ Tester.py
@@ -1,6 +1,16 @@
-import Compresser
+import unittest
+from Compresser import Compresser
 
-text = "Some long piece of text to summarize..."
-compresser = Compresser(text)
-compresser.summarize()
-print(compresser.summary)
+class TestCompresser(unittest.TestCase):
+    def test_summarize_returns_non_empty_string(self):
+        # Call the summarize method with some text
+        text = "Some long piece of text to summarize..."
+        compresser = Compresser(text)
+        compresser.summarize()
+
+        # Assert that the summary is a non-empty string
+        self.assertIsInstance(compresser.summary, str)
+        self.assertGreater(len(compresser.summary), 0)
+
+if __name__ == '__main__':
+    unittest.main()
